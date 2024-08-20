12ff47fa00d1a7183704407fad9bb3064d8be19b
yurannhassengo <yurannhassengo@gmail.com>
2024-08-20 01:52:40 -0700

    Add cors

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes/routes"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-});
-app.listen(3333, () => console.log("Servidor inicializado"));
+app.use((0, cors_1.default)());
+app.use(routes_1.default);
+app.listen(3333, () => console.log("Servidor inicializado......"));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import  express from 'express'
 import routes from './routes/routes';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors());
 
 app.use(routes);
 
